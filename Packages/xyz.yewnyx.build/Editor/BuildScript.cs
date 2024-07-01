1d76fbd98ca507364b5db2b7fd09e4e4770d028f
yewnyx <yewnyx@yewnyx.xyz>
2024-06-30 18:37:23 -0700

    Update build script to properly use build profiles

--- Packages/xyz.yewnyx.build/Editor/BuildScript.cs
+++ Packages/xyz.yewnyx.build/Editor/BuildScript.cs
@@ -1,45 +1,39 @@
 #if UNITY_EDITOR
 using System;
 using System.Linq;
+using JetBrains.Annotations;
 using UnityEditor;
+using UnityEditor.Build.Profile;
 using UnityEditor.Build.Reporting;
 using UnityEngine;
 
 namespace xyz.yewnyx.build
 {
     public static class BuildScript {
-        [MenuItem("yewnyx/Build/Find Build Profiles")]
-        public static void FindBuildProfiles() {
-            var profiles = AssetDatabase.FindAssets("t:UnityEditor.Build.Profile.BuildProfile", new []{"Assets/Settings/Build Profiles"});
-            foreach (var profile in profiles) {
-                Debug.Log(AssetDatabase.GUIDToAssetPath(profile));
-            }
-        }
-        
-        
+        [PublicAPI]
         public static void SwitchProfile() {
-            // Do stuff...?
+            var currentProfile = BuildProfile.GetActiveBuildProfile();
+            Debug.Log("Active Build Profile: " + currentProfile.name);
             EditorApplication.Exit(0);
         }
         
+        [PublicAPI]
         public static void Build() {
-            // get output path from environment args
+            var currentProfile = BuildProfile.GetActiveBuildProfile();
+            Debug.Log("Active Build Profile: " + currentProfile.name);
+            
             var args = Environment.GetCommandLineArgs().ToList();
             var outputPathIndex = args.IndexOf("-outputPath");
             if (outputPathIndex == -1) { throw new Exception("-outputPath not found in command line arguments"); }
             var outputPath = args[outputPathIndex + 1];
-            
-            var buildPlayerOptions = new BuildPlayerOptions {
-                scenes = EditorBuildSettings.scenes.Where(scene => scene.enabled).Select(scene => scene.path).ToArray(),
-                targetGroup = EditorUserBuildSettings.selectedBuildTargetGroup,
-                target = BuildTarget.StandaloneLinux64,
-                subtarget = (int)StandaloneBuildSubtarget.Server,
-                // TODO: Development builds
-                options = BuildOptions.None,
+
+            var bpwpo = new BuildPlayerWithProfileOptions {
                 locationPathName = outputPath,
+                // Rely on build script to launch this with correct build profile
+                buildProfile = currentProfile
             };
 
-            var report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+            var report = BuildPipeline.BuildPlayer(bpwpo);
             var summary = report.summary;
 
             switch (summary.result) {
@@ -52,6 +46,8 @@ namespace xyz.yewnyx.build
                 default:
                     throw new ArgumentOutOfRangeException();
             }
+            
+            EditorApplication.Exit(0);
         }
     }
 }

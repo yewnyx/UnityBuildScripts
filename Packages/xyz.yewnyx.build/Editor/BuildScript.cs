f0a93b7b96d843993cf8b5467f3ec211e5003d0f
yewnyx <yewnyx@yewnyx.xyz>
2024-07-01 23:44:23 -0700

    Update build's bundleVersion with branch and commit for a build, then revert

--- Packages/xyz.yewnyx.build/Editor/BuildScript.cs
+++ Packages/xyz.yewnyx.build/Editor/BuildScript.cs
@@ -1,6 +1,7 @@
 #if UNITY_EDITOR
 using System;
 using System.Linq;
+using System.Text;
 using JetBrains.Annotations;
 using UnityEditor;
 using UnityEditor.Build.Profile;
@@ -27,27 +28,46 @@ namespace xyz.yewnyx.build
             if (outputPathIndex == -1) { throw new Exception("-outputPath not found in command line arguments"); }
             var outputPath = args[outputPathIndex + 1];
 
-            var bpwpo = new BuildPlayerWithProfileOptions {
-                locationPathName = outputPath,
-                // Rely on build script to launch this with correct build profile
-                buildProfile = currentProfile
-            };
+            var branchNameIndex = args.IndexOf("-branch");
+            var commitIndex = args.IndexOf("-commit");
 
-            var report = BuildPipeline.BuildPlayer(bpwpo);
-            var summary = report.summary;
+            var branchName = string.Empty;
+            if (branchNameIndex != -1) { branchName = args[branchNameIndex + 1]; }
 
-            switch (summary.result) {
-                case BuildResult.Succeeded:
-                    Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
-                    break;
-                case BuildResult.Failed:
-                    Debug.Log("Build failed");
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
+            var commit = string.Empty;
+            if (commitIndex != -1) { commit = args[commitIndex + 1]; }
+
+            var cleanVersionString = PlayerSettings.bundleVersion;
+            var versionString = $"{cleanVersionString}-{branchName}-{commit}".Trim('-');
+            PlayerSettings.bundleVersion = versionString;
+
+            try {
+                var bpwpo = new BuildPlayerWithProfileOptions
+                {
+                    locationPathName = outputPath,
+                    // Rely on build script to launch this with correct build profile
+                    buildProfile = currentProfile
+                };
+
+                var report = BuildPipeline.BuildPlayer(bpwpo);
+                var summary = report.summary;
+
+                switch (summary.result)
+                {
+                    case BuildResult.Succeeded:
+                        Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+                        break;
+                    case BuildResult.Failed:
+                        Debug.Log("Build failed");
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                EditorApplication.Exit(0);
+            } finally {
+                PlayerSettings.bundleVersion = versionString;
             }
-            
-            EditorApplication.Exit(0);
         }
     }
 }
